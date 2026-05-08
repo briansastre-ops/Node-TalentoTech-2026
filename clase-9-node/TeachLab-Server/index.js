60329724aee6d9f69dba1cec9f27e0e28a57c209
Brian Sastre <briansastre159@gmail.com>
2026-05-08 17:08:47 -0300

    feat: la clase 10 se subio en la clase 9 para no tener que repetir todo medoficando el index js

--- clase-9-node/TeachLab-Server/index.js
+++ clase-9-node/TeachLab-Server/index.js
@@ -1,21 +1,17 @@
 import express from 'express';
-import cors from 'cors';
-import productRoutes from './routes/products.routes.js';
+const router = express.Router();
 
-const app = express();
-const PORT = 3000;
-
-app.use(cors());
-app.use(express.json());
-
-// Usamos el router modular
-app.use('/api/products', productRoutes);
-
-// Middleware 404
-app.use((req, res) => {
-    res.status(404).json({ error: "Ruta no encontrada en el servidor de BugsBusters" });
+// Ruta HTML: Bienvenida
+router.get('/welcome', (req, res) => {
+    res.send('<h1>🚀 Bienvenido a la sección de Productos</h1>');
 });
 
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor profesional corriendo en http://localhost:${PORT}`);
+// Ruta JSON: Status de la API
+router.get('/status', (req, res) => {
+    res.json({
+        message: "Ruta de productos operativa",
+        database: "Firestore"
+    });
 });
+
+export default router;
